a0e5295004ff5dae97256355442f95da19d3f8b8
Joey NERSC <joseph.schmidt@utexas.edu>
2024-02-08 14:57:46 -0800

    Added base code for units labeler and dummy example csv look up tables for CGYRO and GENE

---
+++ src/units_labeler.py
@@ -0,0 +1,13 @@
+
+
+
+def units_labeler(input, quantities):
+    # Check if 'quantities' is an instance of str, if so, convert it to a list containing that string.
+    quantities = [quantities] if isinstance(quantities, str) else quantities
+
+    units_dict = {}
+
+    for quant in quantities:
+        pass
+
+    return input, units_dict
